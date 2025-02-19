68cc706161612982fae9f5db374759607810e5d5
lexa <52092946+alexeitkch@users.noreply.github.com>
2025-02-19 17:34:36 +0300

    20250219

--- Lesson_4.5.cpp
+++ Lesson_4.5.cpp
@@ -29,6 +29,7 @@ int main() {
 }*/
 
 /*1.v2*/
+/*
 #include <iostream>
 #include <string.h>
 using namespace std;
@@ -37,7 +38,7 @@ int main() {
     char str[MAXLEN];
     fgets(str, MAXLEN, stdin);
     char* ptr = str;
-    char str1[] = "+-*/%";
+    char str1[] = "+-* /%";
     int sum = 0;
     while(*ptr != '\0') {
         for(int i = 0; i < strlen(str1); i++) {
@@ -49,7 +50,7 @@ int main() {
     printf("%d\n", sum);
     return 0;
 }
-
+*/
 /*2. Вводится строка, которая может содержать пробелы.
  Все символы-цифры, которые в ней встречаются, замените
  символом '0'.  Распечатайте преобразованную строку.
@@ -106,3 +107,208 @@ int main() {
     puts(str);
     return 0;
 }*/
+
+// Lesson_4.5 Дополнительные задания по теме "Строки"
+
+/*4.Пользователь вводит строку, которая состоит из слов, 
+разделенных одним или несколькими пробелами. 
+Пробелы могут быть также перед первым и после последнего слова. 
+С новой строки он вводит символ.
+Найдите и распечатайте все слова, которые начинаются на этот символ. 
+Каждое слово выводится с новой строки.
+Sample Input:
+It’s    better  to be   safe  than   sorry
+s
+Sample Output:
+safe
+sorry*/
+//for visual studio
+/*
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+	const int MAXLEN = 128;
+	char str[MAXLEN];
+	//ввод строки
+	fgets(str, MAXLEN, stdin);
+	char c;
+	//ввод символа
+	c = getchar();
+	char *word, *next = NULL;
+	word = strtok_s(str, " ", &next);
+	while (word != NULL) {
+		if (*word == c)
+			puts(word);
+		word = strtok_s(NULL, " ", &next);
+	}
+	return 0;
+}*/
+/*
+//for stepik
+#include <iostream>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+int main() {
+	const int MAXLEN = 128;
+	char str[MAXLEN];
+	//ввод строки
+	fgets(str, MAXLEN, stdin);
+	char c;
+	//ввод символа
+	cin >> c;
+	char* word;
+	word = strtok(str, " ");
+	while (word != NULL) {
+		if (*word == c)
+			cout << word << endl;
+		word = strtok(NULL, " ");
+	}
+	return 0;
+}*/
+
+/*5. Пользователь вводит строку, которая состоит из слов, 
+разделенных одним или несколькими пробелами. 
+Удалите из строки второе слово. Все пробелы должны остаться.
+Преобразованная строка выводится на консоль.
+Sample Input:
+You   can't  make an omelet without breaking a few eggs
+Sample Output:
+You     make an omelet without breaking a few eggs*/
+//visual studio
+/*
+#include <iostream>
+#include <string.h>
+#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+int main() {
+	const int MAXLEN = 128;
+	char str[MAXLEN];
+	char tmp[MAXLEN];
+	//ввод строки
+	fgets(str, MAXLEN, stdin);
+	strcpy_s(tmp, MAXLEN, str);
+	char* word, *next = NULL;
+	word = strtok_s(tmp, " ", &next);
+	word = strtok_s(NULL, " ", &next);
+	int len = strlen(word);
+	char* s = strstr(str, word);
+	if (s != NULL) {
+		while (*s != '\0') {
+			*s = *(s + len);
+			s++;
+		}
+	}
+	puts(str);
+	return 0;
+}*/
+//stepik
+/*
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+	const int MAXLEN = 128;
+	char str[MAXLEN];
+	char tmp[MAXLEN];
+	//ввод строки
+	fgets(str, MAXLEN, stdin);
+	//копируем строку в tmp
+	strcpy(tmp, str);
+	char* word;
+	word = strtok(tmp, " ");
+	word = strtok(NULL, " ");
+	int len = strlen(word);
+	//поиск лексемы word
+	char* s = strstr(str, word);
+	if (s != NULL) {
+		//сдвиг части строки влево
+		while (*s != '\0') {
+			*s = *(s + len);
+			s++;
+		}
+	}
+	puts(str);
+	return 0;
+}*/
+
+/*6 Пользователь вводит строку, которая состоит из слов, 
+разделенных одним или несколькими пробелами.
+Распечатайте все слова-палиндромы, которые встречаются  в данной строке. 
+Палиндромом называется слово или фраза, которые одинаково читаются слева 
+направо и справа налево. Например: deed (подвиг), level (уровень), sagas (саги, сказания).
+Все прописные буквы перед проверкой переведите в строчные. 
+Для этого можно использовать функцию int tolower(int c), 
+которая преобразует символ к нижнему регистру.
+Sample Input:
+His deed described in Sagas
+Sample Output:
+deed
+sagas
+*/
+//Visual Studio version
+/*
+#include <iostream>
+using namespace std;
+int main() {
+	const int MAXLEN = 128;
+	char str[MAXLEN];
+	//ввод строки
+	fgets(str, MAXLEN, stdin);
+	char* word, *next = NULL;
+	word = strtok_s(str, " ", &next);
+	while (word != NULL) {
+		char bword[MAXLEN];
+		strcpy_s(bword, word);
+		//перевод прописных символов в строчные
+		for (int i = 0; i < strlen(bword); i++) {
+			bword[i] = tolower(bword[i]);
+			word[i] = tolower(word[i]);
+		}
+		//переворачиваем слово
+		for (int i = 0; i < strlen(bword) / 2; i++) {
+			char tmp = bword[i];
+			int a = (strlen(bword) / 2 + 1) - i;
+			bword[i] = bword[a];
+			bword[a] = tmp;
+		}
+		if(!strcmp(word, bword))
+			cout << word << endl;
+		word = strtok_s(NULL, " ", &next);
+	}
+	return 0;
+}*/
+
+//Stepik version
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+	const int MAXLEN = 128;
+	char str[MAXLEN];
+	//ввод строки
+	fgets(str, MAXLEN, stdin);
+	char* word;
+	word = strtok(str, " ");
+	while (word != NULL) {
+		char bword[MAXLEN];
+		strcpy(bword, word);
+		//перевод прописных символов в строчные
+		for (int i = 0; i < strlen(bword); i++) {
+			bword[i] = tolower(bword[i]);
+			word[i] = tolower(word[i]);
+		}
+		//переворачиваем слово
+		for (int i = 0; i < strlen(bword) / 2; i++) {
+			char tmp = bword[i];
+			int a = (strlen(bword) / 2 + 1) - i;
+			bword[i] = bword[a];
+			bword[a] = tmp;
+		}
+		if (!strcmp(word, bword))
+			cout << word << endl;
+		word = strtok(NULL, " ");
+	}
+	return 0;
+}
